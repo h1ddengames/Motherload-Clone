565f11e257da6a2057b81bbc76db4ad854723ac4
h1ddengames <skarim3000@gmail.com>
2020-04-25 00:51:23 -0700

    Updated actions, added splash and quit screen
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -9,7 +9,8 @@ using UnityEngine;
 namespace h1ddengames {
     public class Splash : MonoBehaviour {
         #region Exposed Fields
-        public ModalWindowManager modalWindow;
+        public ModalWindowManager splashModalWindow;
+        public ModalWindowManager quitModalWindow;
         #endregion
 
         #region Private Fields
@@ -19,11 +20,23 @@ namespace h1ddengames {
         #endregion
 
         #region My Methods
+        public void Quit() {
+            Application.Quit(0);
+        }
         #endregion
 
         #region Unity Methods
         private void Start() {
-            modalWindow.OpenWindow();
+            splashModalWindow.gameObject.SetActive(true);
+            splashModalWindow.OpenWindow();
+            quitModalWindow.gameObject.SetActive(false);
+        }
+
+        private void Update() {
+            if(Input.GetKeyDown(KeyCode.Escape)) {
+                quitModalWindow.gameObject.SetActive(true);
+                quitModalWindow.OpenWindow();
+            }
         }
         #endregion
 
