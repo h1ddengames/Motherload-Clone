03c3a2b7f335a4b547150b5701a7b2f84624ccce
h1ddengames <skarim3000@gmail.com>
2020-04-24 23:49:37 -0700

    Added github actions, added assets
    Added Graphy (FPS graph)
    Added Odin Inspector and Serializer
    Added DOTween
    Added Modern UI Pack
    Added Clean Settings UI
    Added TextMeshProUGUI
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,33 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+namespace h1ddengames {
+    public class Splash : MonoBehaviour {
+        #region Exposed Fields
+        public ModalWindowManager modalWindow;
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+        #endregion
+
+        #region My Methods
+        #endregion
+
+        #region Unity Methods
+        private void Start() {
+            modalWindow.OpenWindow();
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
