d23b8af051269e73c091635ab005d33cfa7dcbc1
h1ddengames <skarim3000@gmail.com>
2020-04-26 20:20:52 -0700

    Implemented Remote Config
    Created DebugTool that replaces the Debug class as the default logger.
    Using Remote Config, there are three settings (allowLog, allowWarning, and allowError) that can be changed remotely through the remote config dashboard.
    Depending on if the above settings are enabled, the corresponding Debug messages (Log, LogWarning, LogError) will display.
    Created DebugToolTest to test if DebugTool log messages are displayed properly depending on the value of allowLog, allowWarning, and allowError.
    DebugToolTest also tests if remote config is being pulled at the start of the game as well as if remote configs can be pulled after a certain amount of time.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Test/DebugToolTest.cs
@@ -0,0 +1,35 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+using h1ddengames.Core;
+
+namespace h1ddengames.Test {
+    public class DebugToolTest : MonoBehaviour {
+        #region Exposed Fields
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+        #endregion
+
+        #region My Methods
+        #endregion
+
+        #region Unity Methods
+        private void Update() {
+            DebugTool.Log("Log");
+            DebugTool.LogError("Error");
+            DebugTool.LogWarning("Warning");
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
