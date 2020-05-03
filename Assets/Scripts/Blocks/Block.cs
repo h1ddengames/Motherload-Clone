30659d34aed18253ff3a1641a79a0941d9b5ad1a
h1ddengames <skarim3000@gmail.com>
2020-05-03 02:29:24 -0700

    Added Block Zones
    - Created Block Zones
    - Implemented Block Zone Script Logic
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Blocks/Block.cs
@@ -0,0 +1,120 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+namespace h1ddengames.Block {
+    public class Block : SerializedMonoBehaviour {
+        #region Exposed Fields
+        [SerializeField] private BlockModel blockModel;
+        #endregion
+
+        #region Private Fields
+        private SpriteRenderer blockSpriteRenderer;
+        private AudioSource blockAudioSource;
+        private Animator blockAnimator;
+        #endregion
+
+        #region Getters/Setters/Constructors
+        public BlockModel BlockModel { get => blockModel; set => blockModel = value; }
+        #endregion
+
+        #region My Methods
+        public void OnBlockDestroyed() {
+            if(BlockModel.BlockDestructionAudioClip != null) {
+                blockAudioSource.Play();
+            }
+
+            blockAnimator.SetTrigger("destroyed");
+        }
+        #endregion
+
+        #region Unity Methods
+        private void Start() {
+            if(BlockModel.BlockSprite == null) {
+                Debug.LogError("Sprite reference is missing.");
+            }
+
+            if(blockSpriteRenderer == null) {
+                blockSpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+            }
+
+            if(blockSpriteRenderer.sprite == null) {
+                blockSpriteRenderer.sprite = BlockModel.BlockSprite;
+            }
+
+            if(blockAudioSource == null) {
+                blockAudioSource = GetComponent<AudioSource>();
+
+                if(blockAudioSource != null && BlockModel.BlockDestructionAudioClip != null) {
+                    blockAudioSource.clip = BlockModel.BlockDestructionAudioClip;
+                }
+            }            
+            
+            if(blockAnimator == null) {
+                blockAnimator = GetComponent<Animator>();
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+
+    [Serializable]
+    public class BlockModel {
+        [SerializeField] private Sprite blockSprite;
+        [SerializeField] private int blockLevel;
+        [SerializeField] private Vector2 blockLevelRandomness;
+        [SerializeField] private int blockExperience;
+        [SerializeField] private Vector2 blockExperienceRandomness;
+        [SerializeField] private float timeToDestroy;
+        [SerializeField] private AudioClip blockDestructionAudioClip;
+
+        #region Getters/Setters/Constructors
+        public Sprite BlockSprite { get => blockSprite; set => blockSprite = value; }
+
+        public int BlockLevel {
+            get => blockLevel;
+            set {
+                if(value < 1) {
+                    value = 1;
+                    Debug.LogError($"Block level was set lower than {value}.");
+                }
+
+                if(value > 100) {
+                    value = 100;
+                    Debug.LogError($"Block level was set higher than {value}.");
+                }
+
+                blockLevel = value;
+            }
+        }
+
+        public int BlockExperience {
+            get => blockExperience;
+            set {
+                if(value < -999999) {
+                    value = -999999;
+                    Debug.LogError($"Block experience was set lower than {value}.");
+                }
+
+                if(value > 999999) {
+                    value = 999999;
+                    Debug.LogError($"Block experience was set higher than {value}.");
+                }
+
+                blockExperience = value;
+            }
+        }
+
+        public float TimeToDestroy { get => timeToDestroy; set => timeToDestroy = value; }
+        public AudioClip BlockDestructionAudioClip { get => blockDestructionAudioClip; set => blockDestructionAudioClip = value; }
+        public Vector2 BlockLevelRandomness { get => blockLevelRandomness; set => blockLevelRandomness = value; }
+        public Vector2 BlockExperienceRandomness { get => blockExperienceRandomness; set => blockExperienceRandomness = value; }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Blocks/BlockZone.cs
@@ -0,0 +1,80 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+namespace h1ddengames.Block {
+    [CreateAssetMenu(fileName = "Block Zone", menuName = "h1ddengames/Blocks/New Block Zone")]
+    public class BlockZone : SerializedScriptableObject {
+        #region Exposed Fields
+        [SerializeField] private int lowXPosition; // How far left the blocks should spawn.
+        [SerializeField] private int highXPosition; // How far right the blocks should spawn.
+        [SerializeField] private int lowYPosition; // How far down the blocks should spawn.
+        [SerializeField] private Dictionary<BlockModel, int> blockProbability = new Dictionary<BlockModel, int>();
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+        public int LowXPosition { 
+            get => lowXPosition; 
+            set {
+                if(value < -30) { value = -30; }
+                if(value > -1) { value = -1; }
+                lowXPosition = value; 
+            } 
+        }
+        public int HighXPosition { 
+            get => highXPosition; 
+            set {
+                if(value < 1) { value = 1; }
+                if(value > 30) { value = 30; }
+                highXPosition = value; 
+            } 
+        }
+        public int LowYPosition { 
+            get => lowYPosition; 
+            set {
+                if(value < 1) {
+                    value = 1;
+                }
+                lowYPosition = value; 
+            } 
+        }
+        #endregion
+
+        #region My Methods
+        #endregion
+
+        #region Unity Methods
+        private void OnValidate() {
+            if(lowXPosition < -30) {
+                lowXPosition = -30;
+            } else if(lowXPosition > -1) {
+                lowXPosition = -1;
+            }
+
+            if(highXPosition > 30) {
+                highXPosition = 30;
+            } else if(highXPosition < 1) {
+                highXPosition = 1;
+            }
+
+            if(LowYPosition < 1) {
+                lowYPosition = 1;
+            }
+
+            if(blockProbability.Count < 1) {
+                Debug.LogWarning("Please add blocks to the block probability dictionary.");
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
