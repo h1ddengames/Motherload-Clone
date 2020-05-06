5f55df3c3f2edbb781d21f3a1c40e31020da2256
h1ddengames <skarim3000@gmail.com>
2020-05-05 23:33:47 -0700

    Created temporary save system
    Currently in testing phase. No concrete implementation.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Blocks/BlockGenerator.cs
@@ -0,0 +1,108 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+using h1ddengames.Core;
+
+namespace h1ddengames.Block {
+    public class BlockGenerator : SerializedMonoBehaviour {
+        #region Exposed Fields
+        public GameObject blockPrefab;
+
+        public List<BlockZone> listOfBlockZones = new List<BlockZone>();
+        public List<GameObject> listOfBlocks = new List<GameObject>();
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+        #endregion
+
+        #region My Methods
+
+
+
+
+        //[Button]
+        //public void GenerateBlocks() {
+        //    RemoveAllBlocks();
+
+        //    int additionalDepth = 0;
+
+        //    GameObject blockGameObject;
+        //    Block block;
+        //    Sprite sprite;
+
+        //    for(int currentBlockZone = 0; currentBlockZone < listOfBlockZones.Count; currentBlockZone++) {
+        //        GameObject zoneHolder = new GameObject("Zone " + currentBlockZone);
+        //        zoneHolder.transform.SetParent(transform);
+
+        //        if(currentBlockZone != 0) {
+        //            additionalDepth += listOfBlockZones[currentBlockZone - 1].depth;
+        //        }
+
+        //        for(int currentYPosition = 0; currentYPosition < listOfBlockZones[currentBlockZone].depth; currentYPosition++) {
+        //            for(int currentXPosition = listOfBlockZones[currentBlockZone].leftEdge; currentXPosition < listOfBlockZones[currentBlockZone].rightEdge; currentXPosition++) {
+        //                sprite = WeightedRandomizer.From(listOfBlockZones[currentBlockZone].BlockProbability).TakeOne();
+
+        //                if(!sprite.name.Contains("blank")) {
+        //                    blockGameObject = Instantiate(blockPrefab, new Vector2(currentXPosition, -currentYPosition - additionalDepth), Quaternion.identity, zoneHolder.transform);
+
+        //                    block = blockGameObject.GetComponent<Block>();
+        //                    block.blockSpriteRenderer.sprite = sprite;
+        //                    block.AssignSpriteToBlock();
+
+        //                    listOfBlocks.Add(blockGameObject);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        //[Button]
+        //public void RemoveAllBlocks() {
+        //    for(int i = 0; i < transform.childCount;) {
+        //        if(transform.childCount > 0) {
+        //            DestroyImmediate(transform.GetChild(i).gameObject);
+        //            i = 0;
+        //        } else {
+        //            i++;
+        //        }
+        //    }
+
+        //    listOfBlocks.Clear();
+        //}
+
+        //// 200 Blocks = -10 x, 10 y, 10 d, 3 ms
+        //// 2000 Blocks = -10 x, 10 y, 100 d, 43 ms
+        //// 20000 Blocks = -10 x, 10 y, 1000 d, 4.5 s
+        //// 200000 Blocks = -10 x, 10 y, 10000 d, 54 s
+        //public void GenerateTimedTest() {
+        //    System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+        //    stopWatch.Start();
+
+        //    GenerateBlocks();
+
+        //    stopWatch.Stop();
+        //    System.TimeSpan ts = stopWatch.Elapsed;
+        //    string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        //        ts.Hours, ts.Minutes, ts.Seconds,
+        //        ts.Milliseconds / 10);
+        //    Debug.Log($"Run time: { elapsedTime} generated {listOfBlocks.Count}");
+        //}
+        #endregion
+
+        #region Unity Methods
+        private void Start() {
+
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Core/SerializationTool.cs
@@ -0,0 +1,142 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.IO;
+
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
+
+namespace h1ddengames {
+    public class SerializationTool : SerializedMonoBehaviour {
+        #region Exposed Fields
+        public string savePath = $"C:\\Work\\Projects\\C#\\Motherload-Clone\\Temp\\save.txt";
+        public byte[] bytesAsBytesArray;
+        public EntityStats stats = new EntityStats();
+        public ImprovedEntityStats improvedStats = new ImprovedEntityStats();
+        public ImprovedEntityStats otherImprovedStats = new ImprovedEntityStats();
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+
+        #endregion
+
+        #region My Methods
+        [Button]
+        public void SaveState() {
+            byte[] bytes = SerializationUtility.SerializeValue(stats, DataFormat.Binary);
+            bytesAsBytesArray = bytes;
+            Debug.Log(bytes.ToString());
+        }
+
+        [Button]
+        public void LoadState() {
+            stats = SerializationUtility.DeserializeValue<EntityStats>(bytesAsBytesArray, DataFormat.Binary);
+        }
+
+        [Button]
+        public void SaveStateWithFile() {
+            byte[] bytes = SerializationUtility.SerializeValue(stats, DataFormat.Binary);
+            File.WriteAllBytes(savePath, bytes);
+        }
+
+        [Button]
+        public void LoadStateWithFile() {
+            byte[] bytes = File.ReadAllBytes(savePath);
+            stats = SerializationUtility.DeserializeValue<EntityStats>(bytes, DataFormat.Binary);
+        }
+
+        [Button]
+        public void AddStats() {
+            Debug.Log(improvedStats.hp.GetStat() + otherImprovedStats.hp.GetStat());
+        }
+        #endregion
+
+        #region Unity Methods
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+
+    public class EntityStats {
+        public int hp;
+        public int mp;
+        public float strength;
+        public float intelligence;
+        public float luck;
+        public float dexterity;
+    }
+
+    public class ImprovedEntityStats {
+        public Stat<int> hp;
+        public Stat<int> mp;
+        public Stat<float> strength;
+        public Stat<float> intelligence;
+        public Stat<float> luck;
+        public Stat<float> dexterity;
+    }
+
+    public class Stat<T> : IStat<T> {
+        public T value;
+
+        public IStat<T> IncrementStat(T amount) {
+            GenericMath<T>.Add(value, amount);
+            return this;
+        }
+
+        public IStat<T> DecrementStat(T amount) {
+            GenericMath<T>.Subtract(value, amount);
+            return this;
+        }
+
+        public T GetStat() {
+            return value;
+        }
+
+        public IStat<T> SetStat(T value) {
+            this.value = value;
+            return this;
+        }
+    }
+
+
+    public interface IStat<T> {
+        IStat<T> IncrementStat(T amount);
+        IStat<T> DecrementStat(T amount);
+        T GetStat();
+        IStat<T> SetStat(T value);
+    }
+
+    public class GenericMath<T> {
+        public static T Add(T value1, T value2) {
+            dynamic a = value1;
+            dynamic b = value2;
+            return a + b;
+        }
+
+        public static T Subtract(T value1, T value2) {
+            dynamic a = value1;
+            dynamic b = value2;
+            return a - b;
+        }
+
+        public static T Multiply(T value1, T value2) {
+            dynamic a = value1;
+            dynamic b = value2;
+            return a * b;
+        }
+
+        public static T Divide(T value1, T value2) {
+            dynamic a = value1;
+            dynamic b = value2;
+            return a / b;
+        }
+    }
+}
