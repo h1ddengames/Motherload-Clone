70b0c3462305bba9ffe7385e89f10e78c846ac1b
h1ddengames <skarim3000@gmail.com>
2020-05-06 23:38:14 -0700

    Updated save system
    Switched from binary to JSON format for the save data.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

--- Assets/Scripts/Core/SerializationTool.cs
+++ Assets/Scripts/Core/SerializationTool.cs
@@ -2,59 +2,47 @@
 
 using System;
 using System.IO;
-
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Michsky.UI.ModernUIPack;
 using UnityEngine;
 using Sirenix.Serialization;
-using Sirenix.Utilities;
 
 namespace h1ddengames {
     public class SerializationTool : SerializedMonoBehaviour {
         #region Exposed Fields
-        public string savePath = $"C:\\Work\\Projects\\C#\\Motherload-Clone\\Temp\\save.txt";
-        public byte[] bytesAsBytesArray;
+        public string savePath = "save.txt";
+        public List<EntityStats> allStats = new List<EntityStats>();
         public EntityStats stats = new EntityStats();
-        public ImprovedEntityStats improvedStats = new ImprovedEntityStats();
-        public ImprovedEntityStats otherImprovedStats = new ImprovedEntityStats();
         #endregion
 
         #region Private Fields
         #endregion
 
         #region Getters/Setters/Constructors
-
         #endregion
 
         #region My Methods
-        [Button]
-        public void SaveState() {
-            byte[] bytes = SerializationUtility.SerializeValue(stats, DataFormat.Binary);
-            bytesAsBytesArray = bytes;
-            Debug.Log(bytes.ToString());
-        }
-
-        [Button]
-        public void LoadState() {
-            stats = SerializationUtility.DeserializeValue<EntityStats>(bytesAsBytesArray, DataFormat.Binary);
+        public void Start() {
+            Debug.Log($"{Application.persistentDataPath}/{savePath}");
         }
 
         [Button]
         public void SaveStateWithFile() {
-            byte[] bytes = SerializationUtility.SerializeValue(stats, DataFormat.Binary);
-            File.WriteAllBytes(savePath, bytes);
+            if(string.IsNullOrEmpty(savePath)) {
+                savePath = "save.txt";
+            }
+
+            string fullSavePath = $"{Application.persistentDataPath}/{savePath}";
+            byte[] bytes = SerializationUtility.SerializeValue(allStats, DataFormat.JSON);
+            File.WriteAllBytes(fullSavePath, bytes);
         }
 
         [Button]
         public void LoadStateWithFile() {
-            byte[] bytes = File.ReadAllBytes(savePath);
-            stats = SerializationUtility.DeserializeValue<EntityStats>(bytes, DataFormat.Binary);
-        }
-
-        [Button]
-        public void AddStats() {
-            Debug.Log(improvedStats.hp.GetStat() + otherImprovedStats.hp.GetStat());
+            string fullSavePath = $"{Application.persistentDataPath}/{savePath}";
+            byte[] bytes = File.ReadAllBytes(fullSavePath);
+            allStats = SerializationUtility.DeserializeValue<List<EntityStats>>(bytes, DataFormat.JSON);
         }
         #endregion
 
@@ -65,6 +53,7 @@ namespace h1ddengames {
         #endregion
     }
 
+    [Serializable]
     public class EntityStats {
         public int hp;
         public int mp;
@@ -72,71 +61,8 @@ namespace h1ddengames {
         public float intelligence;
         public float luck;
         public float dexterity;
-    }
-
-    public class ImprovedEntityStats {
-        public Stat<int> hp;
-        public Stat<int> mp;
-        public Stat<float> strength;
-        public Stat<float> intelligence;
-        public Stat<float> luck;
-        public Stat<float> dexterity;
-    }
-
-    public class Stat<T> : IStat<T> {
-        public T value;
-
-        public IStat<T> IncrementStat(T amount) {
-            GenericMath<T>.Add(value, amount);
-            return this;
-        }
-
-        public IStat<T> DecrementStat(T amount) {
-            GenericMath<T>.Subtract(value, amount);
-            return this;
-        }
-
-        public T GetStat() {
-            return value;
-        }
-
-        public IStat<T> SetStat(T value) {
-            this.value = value;
-            return this;
-        }
-    }
-
-
-    public interface IStat<T> {
-        IStat<T> IncrementStat(T amount);
-        IStat<T> DecrementStat(T amount);
-        T GetStat();
-        IStat<T> SetStat(T value);
-    }
-
-    public class GenericMath<T> {
-        public static T Add(T value1, T value2) {
-            dynamic a = value1;
-            dynamic b = value2;
-            return a + b;
-        }
-
-        public static T Subtract(T value1, T value2) {
-            dynamic a = value1;
-            dynamic b = value2;
-            return a - b;
-        }
-
-        public static T Multiply(T value1, T value2) {
-            dynamic a = value1;
-            dynamic b = value2;
-            return a * b;
-        }
-
-        public static T Divide(T value1, T value2) {
-            dynamic a = value1;
-            dynamic b = value2;
-            return a / b;
-        }
+        public int min;
+        public int max;
+        public bool isSet;
     }
 }
