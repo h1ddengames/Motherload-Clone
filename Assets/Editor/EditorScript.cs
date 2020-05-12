8b05a2141eadf8ac2a3d649940ad52e65d073c92
h1ddengames <skarim3000@gmail.com>
2020-05-11 23:03:48 -0700

    Added Editor asmdef
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Editor/EditorScript.cs
@@ -0,0 +1,27 @@
+﻿// Created by h1ddengames
+
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace h1ddengames {
+    public class EditorScript : Editor {
+        #region Exposed Fields
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region Getters/Setters/Constructors
+        #endregion
+
+        #region My Methods
+        #endregion
+
+        #region Unity Methods
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
