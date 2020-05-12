d4a93c214ab6f9f3da3a07bdc1ed7a73fef8da40
h1ddengames <skarim3000@gmail.com>
2020-05-11 21:08:41 -0700

    Added testing, Updated Tools, New Assemblies
    Created External Assemblies asmdef for all asset store scripts.
    Created Interal Assemblies asmdef for all project related - self created sripts.
    Created Tests Assemblies asmdef for all testing related scripts.
    
    Added automated testing to the CI/CD pipeline.
    Added test scripts.
    Added more extension methods to Tools.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

--- Assets/Scripts/Extensions/Tools.cs
+++ Assets/Scripts/Extensions/Tools.cs
@@ -17,36 +17,42 @@ namespace h1ddengames.Extensions {
 
         #region Helper Methods
         #region Random Collection Value
-        // public static T GetRandomArrayValue<T>(T[] array) => array[GetRandomInt(0, array.Length)];
-        // public static T GetRandomListValue<T>(List<T> list) => list[GetRandomInt(0, list.Count)];
-        // public static T1 GetRandomDictionaryKey<T1, T2>(Dictionary<T1, T2> dictionary) => dictionary.ElementAt(GetRandomInt(0, dictionary.Count)).Key;
-        // public static T2 GetRandomDictionaryValue<T1, T2>(Dictionary<T1, T2> dictionary) => dictionary.ElementAt(GetRandomInt(0, dictionary.Count)).Value;
-        // public static string GetRandomCharacter(this string str) => str[GetRandomInt(0, str.Length)].ToString();
-
+        // Validated. Usage: Tools.GetRandomEnumValue<RarityEnum>();
         public static T GetRandomEnumValue<T>() {
             var enumValues = Enum.GetValues(typeof(T));
             return (T) enumValues.GetValue(GetRandomInt(enumValues.Length));
         }
-        
+
+        // Validated. Usage: RarityEnumArray.GetRandomValue();
         public static T GetRandomValue<T>(this T[] array) => array[GetRandomInt(0, array.Length)];
 
+        // Validated. Usage: RarityEnumList.GetRandomValue();
         public static T GetRandomValue<T>(this List<T> list) => list[GetRandomInt(0, list.Count)];
 
+        // Validated. Usage: RarityEnumDictionary.GetRandomKey();
         public static T2 GetRandomValue<T1, T2>(this Dictionary<T1, T2> dictionary) => dictionary.ElementAt(GetRandomInt(0, dictionary.Count)).Value;
 
+        // Validated. Usage: RarityEnumDictionary.GetRandomValue();
         public static T1 GetRandomKey<T1, T2>(this Dictionary<T1, T2> dictionary) => dictionary.ElementAt(GetRandomInt(0, dictionary.Count)).Key;
 
+        // Validated. Usage: "somelongstringherecangeneratearandomcharacter".GetRandomValue();
         public static string GetRandomValue(this string str) => str[GetRandomInt(0, str.Length)].ToString();
+
+        // Validated. Usage: 1679654984.GetRandomValue();
         public static int GetRandomValue(this int value) => int.Parse(value.ToString()[GetRandomInt(0, value.ToString().Length)].ToString());
         #endregion
 
         #region Random Value
+        // Validated. Usage: Tools.GetRandomInt();
         public static int GetRandomInt() => random.Value.Next();
 
+        // Validated. Usage: Tools.GetRandomInt(5000000);
         public static int GetRandomInt(int max) => random.Value.Next(max);
 
+        // Validated. Usage: Tools.GetRandomInt(25, 50);
         public static int GetRandomInt(int min, int max) => random.Value.Next(min, max);
 
+        // Validated. Usage: Tools.GetRandomFloat();
         public static float GetRandomFloat() {
             float first = random.Value.Next();
             float second = random.Value.Next();
@@ -57,16 +63,55 @@ namespace h1ddengames.Extensions {
                 return first / second;
             }
         }
+
+        // Validated. Usage: Tools.GetRandomFloat(50);
+        public static float GetRandomFloat(int max) {
+            float first = random.Value.Next(max);
+            float second = random.Value.Next(max);
+
+            if(first > second) {
+                return second / first;
+            } else {
+                return first / second;
+            }
+        }
+
+        // Validated. Usage: Tools.GetRandomFloat(25, 50);
+        public static float GetRandomFloat(int min, int max) {
+            float first = random.Value.Next(min, max);
+            float second = random.Value.Next(min, max);
+
+            if(first > second) {
+                return second / first;
+            } else {
+                return first / second;
+            }
+        }
+
+        // Not Validated. Usage: 13.2354667.ToDecimalPlaces(3); --> 13.235
+        public static float ToDecimalPlaces(this float value, decimal places) {
+            if(places > 1) {
+                string[] arr = value.ToString().Split('.');
+                StringBuilder decimalPart = new StringBuilder();
+
+                if(arr[1].Length >= places) {
+                    for(int i = 0; i < places; i++) {
+                        decimalPart.Append(arr[1].ToCharArray()[i]);
+                    }
+                } else {
+                    for(int i = 0; i < arr[1].Length - 1; i++) {
+                        decimalPart.Append(arr[1].ToCharArray()[i]);
+                    }
+                }
+
+                return float.Parse(arr[0] + "." + decimalPart.ToString());
+            } else {
+                return value;
+            }
+        }
         #endregion
 
         #region Data Generator
-        //public static string GetRandomString(int length) {
-        //    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-        //    return new string(Enumerable.Repeat(chars, length)
-        //      .Select(s => s[GetRandomInt(s.Length)]).ToArray());
-        //}
-
         // Random People Names
 
         // Random Location Names
@@ -114,15 +159,112 @@ namespace h1ddengames.Extensions {
         #endregion
 
         #region Converters
-        // int i = "123".ChangeType<int>();
-        public static T ChangeType<T>(this object obj) {
+        // Validated. Usage: int i = "123".ChangeType<int>(); --> "123" (string) becomes 123 (int)
+        public static T ChangeType<T>(this object obj) where T : IConvertible {
             return (T) Convert.ChangeType(obj, typeof(T));
+        }
+
+        public static T ToEnum<T>(this string value) {
+            return (T) Enum.Parse(typeof(T), value, true);
+        }
+
+        public static T ToEnum<T>(this int value) {
+            var name = Enum.GetName(typeof(T), value);
+            return name.ToEnum<T>();
+        }
+
+        public static string[] EnumNamesToArray<T>() {
+            return Enum.GetNames(typeof(T)).ToArray();
+        }
+
+        public static int[] EnumValuesToArray<T>() {
+            T[] enumArray = (T[]) Enum.GetValues(typeof(T));
+            int[] intArray = new int[enumArray.Length];
+
+            for(int i = 0; i < enumArray.Length; i++) {
+                intArray[i] = enumArray[i].ChangeType<int>();
+            }
+
+            return intArray;
+        }
+
+        public static List<T> EnumNamesToList<T>() {
+            return Enum.GetNames(typeof(T)).Cast<T>().ToList();
+        }
+
+        public static List<T> EnumValuesToList<T>() {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToList();
+        }
+
+        public static string ToReadableKeys<T1, T2>(this Dictionary<T1, T2> dictionary) {
+            return ToReadable(dictionary.Keys.ToArray(), ",");
+        }
+
+        public static string ToReadableKeys<T1, T2>(this Dictionary<T1, T2> dictionary, string separator) {
+            return ToReadable(dictionary.Keys.ToArray(), separator);
+        }
+
+        public static string ToReadableValues<T1, T2>(this Dictionary<T1, T2> dictionary) {
+            return ToReadable(dictionary.Values.ToArray(), ",");
+        }
+
+        public static string ToReadableValues<T1, T2>(this Dictionary<T1, T2> dictionary, string separator) {
+            return ToReadable(dictionary.Values.ToArray(), separator);
+        }
+
+        public static string ToReadable<T>(this List<T> list) {
+            return ToReadable(list, ",");
+        }
+
+        public static string ToReadable<T>(this List<T> list, string separator) {
+            return ToReadable<T>(list.ToArray(), separator);
+        }
+
+        public static string ToReadable<T>(this T[] array) {
+            return ToReadable<T>(array, ",");
+        }
+
+        public static string ToReadable<T>(this T[] array, string separator) {
+            StringBuilder stringBuilder = new StringBuilder();
+            if(array.Length > 0) {
+                foreach(var item in array) {
+                    stringBuilder.Append(item + $"{separator} ");
+                }
+                return stringBuilder.ToString();
+            } else {
+                return "";
+            }
         }
         #endregion
 
 
         #region Unsorted
+        // "h1ddengames".Capitalize --> "H1ddengames"
+        public static string Capitalize(this string word) {
+            return word[0].ToString().ToUpper() + word.Substring(1);
+        }
 
+        public static string ReverseString(this string str) {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        // allowAdd = true
+        // peopleList.AddIf(allowAdd, "h1ddengames");
+        public static void AddIf<T>(this ICollection<T> collection, Func<bool> predicate, T item) {
+            if(predicate.Invoke()) {
+                collection.Add(item);
+            }
+        }
+
+        // People.AddIf(p => p.Age >= 8, person);
+        public static void AddIf<T>(this ICollection<T> collection, Func<T, bool> predicate, T item) {
+            if(predicate.Invoke(item))
+                collection.Add(item);
+        }
         #endregion
 
         #region Timer
---
+++ Assets/Tests/ToolsTestBaseScript.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Tests {
+    public class ToolsTestBaseScript {
+        public int iterations = 50;
+
+        public enum RarityEnum {
+            None = 0,
+            Common = 10,
+            Rare = 20,
+            Epic = 30,
+            Unique = 40,
+            Legendary = 50,
+            Ancient = 60,
+            OneOfAKind = 70
+        }
+
+        public Dictionary<RarityEnum, int> RarityEnumDictionary = new Dictionary<RarityEnum, int>() {
+            { RarityEnum.None, 0},
+            { RarityEnum.Common, 10 },
+            { RarityEnum.Rare, 20 },
+            { RarityEnum.Epic, 30 },
+            { RarityEnum.Unique, 40},
+            { RarityEnum.Legendary, 50 },
+            { RarityEnum.Ancient, 60 },
+            { RarityEnum.OneOfAKind, 70 }
+        };
+
+        public string[] RarityEnumArray = new string[] {
+            "None", "Common", "Rare", "Epic", "Unique", "Legendary", "Ancient", "OneOfAKind"
+        };
+
+        public List<string> RarityEnumList = new List<string>() {
+            "None", "Common", "Rare", "Epic", "Unique", "Legendary", "Ancient", "OneOfAKind"
+        };
+    }
+}
